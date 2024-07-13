e013d7f1d717ecc6a66e1cf116ec6631b5933b8b
stephaniecui <stephanie.cui23@imperial.ac.uk>
2024-07-13 12:44:30 +0100

    Update urls.py

--- chatbot_project/urls.py
+++ chatbot_project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from chatbot_app import views as chatbot_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
