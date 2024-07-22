8e794c272c02f5be94b33f40e329ea0bf6090f46
stephaniecui <stephanie.cui23@imperial.ac.uk>
2024-07-22 22:48:31 +0100

    Update urls.py

--- chatbot_app/urls.py
+++ chatbot_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, chatbot_response
 
 urlpatterns = [
     path('', index, name='index'),  # Simple Django view for root
+    path('response/', chatbot_response, name='chatbot_response'),
 ]
