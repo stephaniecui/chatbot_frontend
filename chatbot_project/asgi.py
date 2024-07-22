472b6e2719ad3a76cbf19a9bb6cdbc8e08675e36
stephaniecui <stephanie.cui23@imperial.ac.uk>
2024-07-22 22:36:24 +0100

    Update asgi.py

--- chatbot_project/asgi.py
+++ chatbot_project/asgi.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.http import AsgiHandler
 from django.urls import path
 from chatbot_app.views import fastapi_app
 
